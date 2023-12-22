eea59123fd7314c3e96946440782b702b6766713
Crispy Strawberry <crispy-strawberry102938@outlook.com>
2023-12-22 23:12:33 +0530

    Change structure to store `Allocator` better inside `Headers`

--- src/headers.zig
+++ src/headers.zig
@@ -6,13 +6,15 @@ const Allocator = std.mem.Allocator;
 /// the keys and values added to it by allocating
 /// using the given allocator.
 pub const Headers = struct {
-    map: std.StringHashMap([]const u8),
+    allocator: Allocator,
+    map: std.StringHashMapUnmanaged([]const u8),
     allocated_size: usize,
 
     /// Initialized a new header structure
     pub fn init(allocator: Allocator) Headers {
         return Headers{
-            .map = std.StringHashMap([]const u8).init(allocator),
+            .allocator = allocator,
+            .map = .{},
             .allocated_size = 0,
         };
     }
@@ -20,10 +22,10 @@ pub const Headers = struct {
     /// Sets `key` to `value`. All keys and values are allocated using the allocator;
     pub fn set(self: *Headers, key: []const u8, value: []const u8) Allocator.Error!void {
         self.allocated_size += key.len + value.len + 3; // 3 is for the colon, space and newline
-        const owned_key = try self.map.allocator.dupe(u8, key);
-        const owned_value = try self.map.allocator.dupe(u8, value);
+        const owned_key = try self.allocator.dupe(u8, key);
+        const owned_value = try self.allocator.dupe(u8, value);
 
-        try self.map.put(owned_key, owned_value);
+        try self.map.put(self.allocator, owned_key, owned_value);
     }
 
     pub fn get(self: *Headers, key: []const u8) ?[]const u8 {
@@ -42,10 +44,10 @@ pub const Headers = struct {
         while (iterator.next()) |entry| {
             const key = entry.key_ptr.*;
             const value = entry.value_ptr.*;
-            self.map.allocator.free(key);
-            self.map.allocator.free(value);
+            self.allocator.free(key);
+            self.allocator.free(value);
         }
 
-        self.map.deinit();
+        self.map.deinit(self.allocator);
     }
 };
